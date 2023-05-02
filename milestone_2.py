4f87be4e26fe4d4c0ae952dc180f497a1cc359e6
learningcoding-lab <smontaha.chowdhury@gmail.com>
2023-05-02 15:23:57 +0100

    word list was created and user input was checked

--- milestone_2.py
+++ milestone_2.py
@@ -1,28 +1,56 @@
-#create a word list and print its output
-word_list = ['banana', 'apple', 'orange', 'kiwi', 'mango']
+#%%
+# Step 1
+word_list = ["apple", "banana", "orange", "mango", "pineapple"]
+
+# Step 3
 print(word_list)
 
-#import the function random
+# %%
+# Step 1
 import random
 
-#import the function random and its choice function
-word = random.choice(word_list)
+# Step 2
+fruits = ["apple", "banana", "orange", "mango", "pineapple"]
 
-#print the final output
+# Step 3
+word = random.choice(fruits)
+
+# Step 4
 print(word)
+# %%
+# Step 1
+import random
 
-#ask the user to enter a single letter
-#assigning this input to guess
-guess = input('Please enter a single letter: ')
+# Step 2
+fruits = ["apple", "banana", "orange", "mango", "pineapple"]
 
-#Create an if statement that checks if the length of the input is equal to 1 and the input is alphabetical.
-if len(guess) == 1 and guess.isalpha() is True:
-    print('Good Guess!')
+# Step 3
+word = random.choice(fruits)
 
+# Step 4
+guess = input("Guess a fruit: ")
+
+# Step 5
+print("The random fruit was:", word)
+print("Your guess was:", guess)
+# %%
+# Step 1
+import random
+
+# Step 2
+fruits = ["apple", "banana", "orange", "mango", "pineapple"]
+
+# Step 3
+word = random.choice(fruits)
+
+# Step 4
+guess = input("Guess a fruit: ")
+
+# Step 5
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
 else:
-    print("Oops! That's not a valid input!")
+    print("Oops! That is not a valid input.")
 
 print("The random fruit was:", word)
 print("Your guess was:", guess)
-
-
